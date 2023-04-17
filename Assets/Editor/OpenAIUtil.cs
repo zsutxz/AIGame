4988e0d964e000efaefb263b442fb76d1af58ef4
tanxz <zsutxz@gmail.com>
2023-04-17 19:55:56 +0800

    firrst commit

---
+++ Assets/Editor/OpenAIUtil.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Text;
+
+namespace AICommand
+{
+
+    static class OpenAIUtil
+    {
+        static string CreateChatRequestBody(string prompt)
+        {
+            var msg = new OpenAI.RequestMessage();
+            msg.role = "user";
+            msg.content = prompt;
+
+            var req = new OpenAI.Request();
+            req.model = "gpt-3.5-turbo";
+            req.messages = new[] { msg };
+
+            // string jsstr = "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"user\",\"content\":\"how old are you\"}]}";
+            return JsonUtility.ToJson(req);
+        }
+
+        public static string InvokeChat(string prompt)
+        {
+            var settings = AICommandSettings.instance;
+
+            // // POST
+            // using var post = UnityWebRequest.Post(OpenAI.Api.Url, CreateChatRequestBody(prompt), "application/json");
+            // // Request timeout setting
+            // post.timeout = settings.timeout;
+            // // API key authorization
+            // post.SetRequestHeader("Authorization", "Bearer " + settings.apiKey);
+
+
+            //another post way
+            var post = new UnityWebRequest(OpenAI.Api.Url, "POST");
+
+            post.timeout = settings.timeout;
+            post.SetRequestHeader("Content-Type", "application/json");
+            post.SetRequestHeader("Authorization", string.Format("Bearer {0}", settings.apiKey));
+
+            string str = CreateChatRequestBody(prompt);
+            Debug.Log(str);
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(str);
+            post.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            post.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+
+            // Request start
+            var req = post.SendWebRequest();
+
+            // Progress bar (Totally fake! Don't try this at home.)
+            for (var progress = 0.0f; !req.isDone; progress += 0.01f)
+            {
+                EditorUtility.DisplayProgressBar("AI Command", "Generating...", progress);
+                System.Threading.Thread.Sleep(100);
+                progress += 0.01f;
+            }
+
+            EditorUtility.ClearProgressBar();
+
+            if (post.isHttpError || post.isNetworkError)
+            {
+                Debug.Log("isHttpError:"+post.isHttpError);
+                Debug.Log("isNetworkError:"+post.isNetworkError);
+                Debug.Log(post.error);
+            }
+            Debug.Log(post.downloadHandler.text);
+
+            // Response extraction
+            var json = post.downloadHandler.text;
+            var data = JsonUtility.FromJson<OpenAI.Response>(json);
+
+            return data.choices[0].message.content;
+        }
+    }
+
+} // namespace AICommand
